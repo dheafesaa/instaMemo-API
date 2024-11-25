c7e4b54f18fba2cfc431bc4f664456b3b23d8936
Dhea Fesa Athallah <dhea.athallah@ist.id>
2024-11-25 14:33:59 +0700

    refactor code

--- controllers/authentication/login-memo.mjs
+++ controllers/authentication/login-memo.mjs
@@ -1,27 +1,7 @@
-import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  query,
-  where,
-} from "firebase/firestore/lite";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { collection, getDocs, query, where } from "firebase/firestore/lite";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import bcrypt from "bcrypt";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCaCieh2J7pQWxNu7zSkPBX-tWSu868Fe4",
-  authDomain: "instamemo-55140.firebaseapp.com",
-  projectId: "instamemo-55140",
-  storageBucket: "instamemo-55140.firebasestorage.app",
-  messagingSenderId: "933546212819",
-  appId: "1:933546212819:web:f4ee57b913f62db8700326",
-  measurementId: "G-FTEYJCJ3C1",
-};
-
-const fireInit = initializeApp(firebaseConfig);
-const db = getFirestore(fireInit);
-const auth = getAuth(fireInit);
+import { auth, db } from "../../config/firebase-app.mjs";
 
 export const login = async (req, res) => {
   try {
--- controllers/memo/detail-memo.mjs
+++ controllers/memo/detail-memo.mjs
@@ -1,22 +1,5 @@
-import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  doc,
-  getDoc,
-} from "firebase/firestore/lite";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCaCieh2J7pQWxNu7zSkPBX-tWSu868Fe4",
-  authDomain: "instamemo-55140.firebaseapp.com",
-  projectId: "instamemo-55140",
-  storageBucket: "instamemo-55140.firebasestorage.app",
-  messagingSenderId: "933546212819",
-  appId: "1:933546212819:web:f4ee57b913f62db8700326",
-  measurementId: "G-FTEYJCJ3C1",
-};
-
-const fireInit = initializeApp(firebaseConfig);
-const db = getFirestore(fireInit);
+import { doc, getDoc } from "firebase/firestore/lite";
+import { db } from "../../config/firebase-app.mjs";
 
 export const getMemoDetail = async (req, res) => {
   try {
